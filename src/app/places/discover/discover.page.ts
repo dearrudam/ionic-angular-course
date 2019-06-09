1d921a5a5c34475cca9962d114f0350f2bf08140
Maximillian Arruda <dearrudam@gmail.com>
2019-06-08 21:26:17 -0300

    Section 7: Open + Closing the SideDrawer

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-discover',
@@ -11,11 +12,17 @@ export class DiscoverPage implements OnInit {
 
   loadedPlaces: Place[];
 
-  constructor(private placesServices: PlacesService) { }
+  constructor(
+    private menuController: MenuController,
+    private placesServices: PlacesService) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesServices.places;
     console.log(this.loadedPlaces);
+  }
+
+  onOpenMenu() {
+    this.menuController.toggle();
   }
 
 }
