e360f9dde960cef6f3206c5a513873134c691e2f
Maximillian Arruda <dearrudam@gmail.com>
2019-06-18 21:18:03 -0300

    Section 10: Finishing the New Offer Form

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -30,7 +30,7 @@ export class NewOfferPage implements OnInit {
         updateOn: 'blur',
         validators: [Validators.required]
       }),
-      dataTo: new FormControl(null, {
+      dateTo: new FormControl(null, {
         updateOn: 'blur',
         validators: [Validators.required]
       })
